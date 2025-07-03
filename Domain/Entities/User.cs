5eacb99eedaeb633c44b26c067844863ff6a071b
JuanPabloPinillaGuzman <180667131+JuanPabloPinillaGuzman@users.noreply.github.com>
2025-07-02 20:24:32 -0500

    feat: :zap: Add migration

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -13,6 +13,7 @@ namespace Domain.Entities
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? Username { get; set; }
+        public bool IsActive { get; set; } = true;
 
         // Navigation properties
         public ICollection<UserRole>? UserRoles { get; set; }
